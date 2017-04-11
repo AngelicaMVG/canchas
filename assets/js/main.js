50a7861fe7baf8af6356a2269676d045e41cdb02
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-04-11 10:56:18 -0500

    canchas terminadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,3 @@
-// var botonS = document.getElementById('soccerTime');
-// function soccer(){
-//   var canchaSoccer = document.getElementById('cancha');
-//   var porteriaIzquierda= document.createElement('div');
-//   var porteriaDerecha= document.createElement('div');
-//   var centroDeCancha= document.createElement('div');
-//   var circulo= document.createElement('div');
-//   var circuloDePorteriaIzquierda= document.createElement('div');
-//   var circuloDePorteriaDerecha= document.createElement('div');
-//
-//   porteriaIzquierda.setAttribute("id", "porteriaIzquierda");
-//   porteriaDerecha.setAttribute("id","porteriaDerecha");
-//   centroDeCancha.setAttribute("id", "centroDeCancha");
-//   circulo.setAttribute("id","circulo");
-//   circuloDePorteriaIzquierda.setAttribute("id", "circuloDePorteriaIzquierda");
-//   circuloDePorteriaDerecha.setAttribute("id", "circuloDePorteriaDerecha");
-//
-//   canchaSoccer.appendChild(porteriaIzquierda);
-//   canchaSoccer.appendChild(porteriaDerecha);
-//   canchaSoccer.appendChild(centroDeCancha);
-//   canchaSoccer.appendChild(circulo);
-//   canchaSoccer.appendChild(circuloDePorteriaDerecha);
-//   canchaSoccer.appendChild(circuloDePorteriaDerecha);
-//
-//
-// }
+var botonSoccer = document.getElementById('soccer');
+var botonBasquet = document.getElementById('basquet');
+var botonTennis = document.getElementById('tennis');
