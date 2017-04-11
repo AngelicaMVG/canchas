c2d5bb2a1b4062fe485483cfd484d246dad48f44
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-04-10 22:07:08 -0500

    cancha 1 y la mitad de la 2

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// var botonS = document.getElementById('soccerTime');
+// function soccer(){
+//   var canchaSoccer = document.getElementById('cancha');
+//   var porteriaIzquierda= document.createElement('div');
+//   var porteriaDerecha= document.createElement('div');
+//   var centroDeCancha= document.createElement('div');
+//   var circulo= document.createElement('div');
+//   var circuloDePorteriaIzquierda= document.createElement('div');
+//   var circuloDePorteriaDerecha= document.createElement('div');
+//
+//   porteriaIzquierda.setAttribute("id", "porteriaIzquierda");
+//   porteriaDerecha.setAttribute("id","porteriaDerecha");
+//   centroDeCancha.setAttribute("id", "centroDeCancha");
+//   circulo.setAttribute("id","circulo");
+//   circuloDePorteriaIzquierda.setAttribute("id", "circuloDePorteriaIzquierda");
+//   circuloDePorteriaDerecha.setAttribute("id", "circuloDePorteriaDerecha");
+//
+//   canchaSoccer.appendChild(porteriaIzquierda);
+//   canchaSoccer.appendChild(porteriaDerecha);
+//   canchaSoccer.appendChild(centroDeCancha);
+//   canchaSoccer.appendChild(circulo);
+//   canchaSoccer.appendChild(circuloDePorteriaDerecha);
+//   canchaSoccer.appendChild(circuloDePorteriaDerecha);
+//
+//
+// }
