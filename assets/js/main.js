7e4df39728b7fb15c2d619aea2e185ae77edc536
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-04-11 12:14:24 -0500

    ya funcionan las canchas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
-var botonSoccer = document.getElementById('soccer');
+var botonFutbol= document.getElementById('futbol');
 var botonBasquet = document.getElementById('basquet');
 var botonTennis = document.getElementById('tennis');
+
+var canchaFut = document.getElementById('canchaFutbol');
+var canchaBasquet = document.getElementById('canchaBasquet');
+var canchaTennis = document.getElementById('canchaTennis');
+
+quitarCanchas();
+function quitarCanchas(){
+	canchaFut.style.display = "none";
+	canchaBasquet.style.display = "none";
+	canchaTennis.style.display = "none";
+};
+
+function futbol(){
+  quitarCanchas();
+  canchaFutbol.style.display ='block';
+};
+
+function basquet(){
+  quitarCanchas();
+  canchaBasquet.style.display ='block';
+};
+
+function tennis(){
+  quitarCanchas();
+  canchaTennis.style.display = 'block';
+};
+
+botonFutbol.addEventListener("click",futbol);
+botonBasquet.addEventListener("click", basquet);
+botonTennis.addEventListener("click",tennis);
